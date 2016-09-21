6d738bf9f55fecfb15adc7ef9fa5adc2253ddd6f
SiamKing <thesiameseelectric@gmail.com>
2016-09-21 13:22:25 -0700

    added styling to users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,9 @@ class UsersController < ApplicationController
   before_action :user_signed_in?, only: [:index, :show]
 
   def show
-
+    if !user_signed_in?
+      redirect_to new_user_registration_path
+    end
   end
 
   def index
