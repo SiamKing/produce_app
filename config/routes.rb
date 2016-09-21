d896fa6ee76e9b2ce03aea6a7dd15a4660826ace
SiamKing <thesiameseelectric@gmail.com>
2016-09-21 11:28:23 -0700

    added registrations controller and strong params for devise to allow name

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :user_produce
   resources :produce
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
