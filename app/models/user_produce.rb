acfbad16f146bb3ae307dd5681575dc69dc00cf9
SiamKing <thesiameseelectric@gmail.com>
2016-09-21 10:26:23 -0700

    added users controller, views and routes

--- app/models/user_produce.rb
+++ app/models/user_produce.rb
@@ -1,2 +1,5 @@
 class UserProduce < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :produce
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#index'
   resources :user_produce
   resources :produce
   devise_for :users
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
