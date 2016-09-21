6739a0ad233e8c1cc391a0ca18c8ffafb72f6151
SiamKing <thesiameseelectric@gmail.com>
2016-09-20 18:32:39 -0700

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  validates :name, :email, presence: true
+  validates :name, :email, uniqueness: true
+  validates :email, length: { :minimum => 8 }
   has_many :user_produce
   has_many :produce, through: :user_produce
 end
