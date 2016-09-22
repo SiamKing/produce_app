8b7c41125671762a323018591f5e0036ce95df87
SiamKing <thesiameseelectric@gmail.com>
2016-09-21 19:48:09 -0700

    add paths to produce controller

--- app/controllers/produce_controller.rb
+++ app/controllers/produce_controller.rb
@@ -7,4 +7,24 @@ class ProduceController < ApplicationController
   def new
     @produce = Produce.new
   end
+
+  def create
+    @produce = Produce.new(produce_params)
+    if @produce.save
+      redirect_to @produce
+    else
+      render :new
+    end
+
+    def show
+      @produce = Produce.find(params[:id])
+    end
+  end
+
+
+  private
+
+  def private_params
+    params.require(:produce).permit(:name, :image, :content, :expires_in, :content)
+  end
 end
