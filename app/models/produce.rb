5353efb7c643f699d7ae700ee6cd7436f99737e0
SiamKing <thesiameseelectric@gmail.com>
2016-09-22 09:41:59 -0700

    added query interface methods to produce class

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -3,4 +3,12 @@ class Produce < ApplicationRecord
   has_many :produce_items
   has_many :users, through: :produce_items
   mount_uploader :image, ImageUploader
+
+  def self.fruit
+    self.where(kind: 'fruit')
+  end
+
+  def self.veggie
+    self.where(kind: 'veggie')
+  end
 end
