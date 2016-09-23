cec95cbe794ef0a47219668bc5a6cf3b7e164a47
SiamKing <thesiameseelectric@gmail.com>
2016-09-22 17:38:49 -0700

    added methods to UserProduce class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   def show
     redirect_to produce_index_path unless user_signed_in?
     current_user
-    
+    @fruit = current_user.user_produce.user_fruit
+    @veggies = current_user.user_produce.user_veggies
   end
 
   def index
--- app/models/user_produce.rb
+++ app/models/user_produce.rb
@@ -7,4 +7,15 @@ class UserProduce < ApplicationRecord
     self.produce.expires_in - ((Time.now.to_i - self.created_at.to_i) / 86400)
   end
 
+  def self.user_fruit
+    self.select do |user_pro|
+      user_pro.produce.kind == 'fruit'
+    end
+  end
+
+  def self.user_veggies
+    self.select do |user_pro|
+      user_pro.produce.kind == 'veggie'
+    end
+  end
 end
