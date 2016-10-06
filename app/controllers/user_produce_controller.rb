d6facb24dc861cd7a8d7009312970bdd4ff31433
SiamKing <thesiameseelectric@gmail.com>
2016-10-06 09:35:53 -0700

    changed redirect to correct path for user produce controller

--- app/controllers/user_produce_controller.rb
+++ app/controllers/user_produce_controller.rb
@@ -10,12 +10,12 @@ class UserProduceController < ApplicationController
       redirect_to welcome_path
     end
   end
-  
+
   def create
     @user_produce = UserProduce.new(produce_id: params[:format])
     @user_produce.user_id = current_user.id
     if @user_produce.save
-      redirect_to user_path(current_user)
+      redirect_to user_produce_path(current_user)
     else
       redirect_to produce_index_path
     end
