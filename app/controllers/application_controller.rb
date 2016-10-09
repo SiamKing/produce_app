767c89dfab3885fb2bf5b418182e3c26e597dedc
SiamKing <thesiameseelectric@gmail.com>
2016-10-09 10:54:22 -0700

    deleted before action in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :logged_in?, except: :welcome
+
 
   def welcome
     if user_signed_in?
