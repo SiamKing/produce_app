7b3abcfe6b6ac73238ef3c92e645a07ed37256dd
SiamKing <thesiameseelectric@gmail.com>
2016-10-13 14:55:37 -0700

    added helper to clean up juices controller

--- app/controllers/juices_controller.rb
+++ app/controllers/juices_controller.rb
@@ -1,18 +1,9 @@
 class JuicesController < ApplicationController
   before_action :logged_in?
+  include JuicesHelper
+
   def index
-    if params[:user_id]
-      @user = User.find(params[:user_id])
-      @juices = @user.juices
-      @header = "Juices created by #{@user.name}"
-    elsif params[:produce_id]
-      @produce = Produce.find(params[:produce_id])
-      @juices = Juice.juices_with_produce(params[:produce_id])
-      @header = "Juices with #{@produce.name}"
-    else
-      @juices = Juice.all
-      @header = "Fresh Juices"
-    end
+    juice_index_helper(params)
   end
 
   def new
@@ -23,20 +14,7 @@ class JuicesController < ApplicationController
   end
 
   def create
-    @juice = Juice.new(juice_params(:name))
-    if @juice.save
-      @juice.user_id = current_user.id
-      @juice.update_attributes(juice_params(:produce_attributes => [:name, :juice_produce_attributes => :quantity]))
-      if @juice.save
-        redirect_to @juice
-      else
-        flash[:alert] = "Please add ingredients"
-        redirect_to new_juice_path(@juice)
-      end
-    else
-      flash[:alert] = "Please name the juice and add ingredients"
-      redirect_to new_juice_path(@juice)
-    end
+    juice_create_helper(params)
   end
 
   def show
--- app/helpers/juices_helper.rb
+++ app/helpers/juices_helper.rb
@@ -1,2 +1,35 @@
 module JuicesHelper
+
+  def juice_index_helper(params)
+    if params[:user_id]
+      @user = User.find(params[:user_id])
+      @juices = @user.juices.sort_by {|juice| juice.name}
+      @header = "Juices created by #{@user.name}"
+    elsif params[:produce_id]
+      @produce = Produce.find(params[:produce_id])
+      @juices = Juice.juices_with_produce(params[:produce_id]).sort_by {|juice| juice.name}
+      @header = "Juices with #{@produce.name}"
+    else
+      @juices = Juice.all.sort_by {|juice| juice.name}
+      @header = "Fresh Juices"
+    end
+  end
+
+  def juice_create_helper(params)
+    @juice = Juice.new(juice_params(:name))
+    if @juice.save
+      @juice.user_id = current_user.id
+      @juice.update_attributes(juice_params(:produce_attributes => [:name, :juice_produce_attributes => :quantity]))
+      if @juice.save
+        redirect_to @juice
+      else
+        flash[:alert] = "Please add ingredients"
+        redirect_to new_juice_path(@juice)
+      end
+    else
+      flash[:alert] = "Please name the juice and add ingredients"
+      redirect_to new_juice_path(@juice)
+    end
+  end
+
 end
