5256abb954b3961bca8add050df8107850f8d978
SiamKing <thesiameseelectric@gmail.com>
2016-11-28 11:17:26 -0800

    changed show action in juices controller

--- app/controllers/juices_controller.rb
+++ app/controllers/juices_controller.rb
@@ -31,7 +31,7 @@ class JuicesController < ApplicationController
     @juice = Juice.find(params[:id])
     @juice_produce = @juice.juice_produce
     @last_juice_id = Juice.last.id
-    @juice.comments.build
+    @juice_comments = @juice.comments
     respond_to do |f|
       f.html { render :show }
       f.json { render json: @juice }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :juice
+  validates :content, :user_id, :juice_id, presence: true
 end
