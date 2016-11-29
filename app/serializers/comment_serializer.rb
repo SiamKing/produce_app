38658361c3452598c4f6454ff6477997723d5c52
SiamKing <thesiameseelectric@gmail.com>
2016-11-29 14:44:11 -0800

    added created_at attribute to CommentsSerializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,5 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :user_id, :juice_id
+  attributes :id, :content, :user_id, :juice_id, :created_at
   has_one :user
   has_one :juice
 end
