7c82a26ba72e2a2264fd06d33b4dc96e465b4194
SiamKing <thesiameseelectric@gmail.com>
2016-12-03 16:10:28 -0800

    fixed Juice prototype for nested attributes and deleted unnecessary code...everything is working

--- app/assets/javascripts/nextPrevious.js
+++ app/assets/javascripts/nextPrevious.js
@@ -42,11 +42,7 @@ function alertClear() {
 function juiceAJAX(id, element) {
   $.get('/juices/' + id + ".json", function(data) {
     var juice = data
-    $('.comments').empty();
-    $('#comment_juice_id').val(juice.id);
-    $.each(juice.comments, function(index, comment) {
-      Comment.success(comment);
-    });
+
 
     // Juice.templateSource = $("#juice-template").html();
     // Juice.template = Handlebars.compile(Juice.templateSource);
@@ -54,22 +50,18 @@ function juiceAJAX(id, element) {
     // var juices = new Juice(juice);
     // var juicesHTML = juices.renderHTML();
     // $('.juice-show').html(juicesHTML);
-    $('.jumbotron-juice').html('');
-    Juice.templateSource = $("#jumbotron-template").html();
+    $('.juice-show').html('');
+    Juice.templateSource = $("#juice-produce-template").html();
     Juice.template = Handlebars.compile(Juice.templateSource);
     var juices = new Juice(juice);
     var juicesHTML = juices.renderHTML();
     $('.juice-show').html(juicesHTML);
 
-    $.each(juice.produce, function(index, juice_produce) {
-      JuiceProduce.templateSource = $("#juice-produce-template").html();
-      JuiceProduce.template = Handlebars.compile(JuiceProduce.templateSource);
-      var juiceProduce = new JuiceProduce(juice_produce);
-      var juiceProduceHTML = juiceProduce.renderHTML();
-
-      $('.juice-produce').append(juiceProduceHTML);
+    $('.comments').empty();
+    $('#comment_juice_id').val(juice.id);
+    $.each(juice.comments, function(index, comment) {
+      Comment.success(comment);
     });
-
   }).fail(function(error) {
     thereIsAnError(id, element);
   });
@@ -79,21 +71,21 @@ Juice.prototype.renderHTML = function() {
   return Juice.template(this);
 }
 
-JuiceProduce.prototype.renderHTML = function() {
-  return JuiceProduce.template(this);
-}
-
 function Juice(attr) {
   this.name = attr.name;
   this.id = attr.id;
   this.userId = attr.user_id;
-}
+  this.userName = attr.user.name
+  this.juiceProduce = attr.produce.map(function(prod, index) {
+                          var produce = {
+                            id: prod.id,
+                            name: prod.name,
+                            image: prod.image.image.thumb.url,
+                            quantity: attr.juice_produce[index].quantity
+                          }
+                          return produce
+                       });
 
-function JuiceProduce(attr) {
-  // this.quantity = attr.quantity;
-  this.produceName = attr.name;
-  this.image = attr.image.image.thumb.url;
-  this.ProduceId = attr.id
 }
 
 Handlebars.registerHelper("debug", function(optionalValue) {
