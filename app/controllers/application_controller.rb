958bc3ceea473fa53e82f15feea20acb9deae126
SiamKing <thesiameseelectric@gmail.com>
2017-12-13 15:28:21 -0800

    Updated flash messages to be used with toastr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  add_flash_types :success, :danger, :notice, :alert, :info
 
   def welcome
     if user_signed_in?
--- app/controllers/user_produce_controller.rb
+++ app/controllers/user_produce_controller.rb
@@ -13,7 +13,7 @@ class UserProduceController < ApplicationController
     @user_produce = UserProduce.new(produce_id: params[:format])
     @user_produce.user_id = current_user.id
     if @user_produce.save
-      flash[:success] = "#{@user_produce.produce.name} was added to your fridge"
+      flash[:success] = "#{@user_produce.produce.name} have been added to your fridge"
       redirect_to user_produce_path(current_user)
     else
       flash[:alert] = "That was not succesful"
@@ -25,7 +25,7 @@ class UserProduceController < ApplicationController
     @user_produce = UserProduce.find(params[:format])
     @user_produce.update_attributes(eaten?: true)
     @user_produce.save
-    flash[:notice] = "Good for you! You ate your #{@user_produce.produce.name}!"
+    flash[:success] = "Good for you! You ate your #{@user_produce.produce.name}!"
     redirect_to user_produce_path(current_user)
   end
 
