840e7180a8f420b7cbbd9eeec3370f19f3184af1
SiamKing <thesiameseelectric@gmail.com>
2017-12-30 12:27:16 -0800

    cleared cache

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,14 +22,15 @@ class User < ApplicationRecord
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
-    end 
+    end
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.name = auth.info.name
+    where(email: auth.info.email).first_or_initialize.tap do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.save
     end
   end
 
