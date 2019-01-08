ed37353194093c1f7b3804b9cad54e70ac512745
SiamKing <thesiameseelectric@gmail.com>
2019-01-08 10:37:41 -0800

    Added ajax functionality for adding and subtracting produce from fridge with remote true and js views

--- app/controllers/user_produce_controller.rb
+++ app/controllers/user_produce_controller.rb
@@ -10,23 +10,28 @@ class UserProduceController < ApplicationController
   end
 
   def create
-    @user_produce = UserProduce.new(produce_id: params[:format])
+    @user_produce = UserProduce.new(produce_id: params[:id])
     @user_produce.user_id = current_user.id
     if @user_produce.save
-      flash[:success] = "#{@user_produce.produce.name} have been added to your fridge"
-      redirect_to user_produce_path(current_user)
+      flash[:success] = "#{@user_produce.produce.name} have been added to your fridge" 
+      render "create.js.erb"
     else
-      flash[:alert] = "That was not succesful"
+      flash[:alert] = "Item was not added. Please try again."
       redirect_to produce_index_path
     end
   end
 
-  def eaten
-    @user_produce = UserProduce.find(params[:format])
+  def update
+    @user_produce = UserProduce.find(params[:id])
     @user_produce.update_attributes(eaten?: true)
-    @user_produce.save
-    flash[:success] = "Good for you! You ate your #{@user_produce.produce.name}!"
-    redirect_to user_produce_path(current_user)
+    if @user_produce.save
+      flash[:success] = "Good for you! You ate your #{@user_produce.produce.name}!"
+      render "update.js.erb", layout: false, content_type: 'text/javascript'
+      # render :json => @user_produce
+    else
+      flash[:alert] = "Whoops. That didn't work. Please try again."
+      redirect_to user_produce_path(current_user)
+    end
   end
 
 end
