755b9b2fadb297656c9032a9e79a4683fa5ccab2
SiamKing <thesiameseelectric@gmail.com>
2019-01-08 10:41:07 -0800

    Updated routes to be more RESTful - user_produce

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   root 'application#welcome'
-  resources :user_produce, :produce, :juice_produce, :juices, :comments
+  resources :user_produce, only: [:show, :update]
+  post "user_produce/:id" => "user_produce#create"
+  resources  :produce, :juice_produce, :juices, :comments
   devise_for :users, :controllers => { registrations: 'registrations', :omniauth_callbacks => "users/omniauth_callbacks" }
   get '/auth/facebook/callback' => 'sessions#create'
   get '/privacy-policy' => 'privacy_policy#privacy_policy'
@@ -15,7 +17,6 @@ Rails.application.routes.draw do
   end
 
   get 'users/:id/produce/index' => 'produce#index', as: :user_user_produce_index
-  post 'eaten' => 'user_produce#eaten'
   get '/welcome' => 'application#welcome'
   get '*path' => redirect('/')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
