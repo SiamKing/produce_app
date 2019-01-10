10e614b485fd5cf42867000a0064bf9d8033811f
SiamKing <thesiameseelectric@gmail.com>
2019-01-10 13:38:03 -0800

    Changed production compiling rule - images not showing in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,7 @@ Rails.application.configure do
   config.serve_static_assets = false
   config.assets.compress = true
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-  config.assets.digest = true
+  config.assets.compile = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
